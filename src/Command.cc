f6d4b1cb29846397c5ad5649ad3ba2ef7b022dbe
David Nguyen <dnguy117@ucr.edu>
2017-05-10 20:50:15 -0700

    Update Command.cc
    Added comments to explain each function.

--- src/Command.cc
+++ src/Command.cc
@@ -9,10 +9,12 @@ void Command::execute () {
     
 }
 
+// sets userInput to input
 void Command::setInput(string input) {
     userInput = input;
 }
 
+// separates string by spaces and places each string block into a vector and char * array
 void Command::parse() {
     
     if (userInput.size() < 1) {
@@ -40,10 +42,12 @@ void Command::parse() {
     return;
 }
 
+// outputs userInput
 void Command::printInput() {
     cout << "Command::printInput(): " << userInput << endl;
 }
 
+// outputs arguments vector
 void Command::printArgs() {
     for (unsigned int i = 0; i < arguments.size(); i++) {
         cout << "Command::printArgs(): " << arguments.at(i) << endl;
@@ -51,28 +55,34 @@ void Command::printArgs() {
     return;
 }
 
+// returns userInput
 string Command::getInput() {
     return userInput;
 }
 
+// returns arguments vector
 vector<string> Command::getArgs() {
     return arguments;
 }
 
+// returns char * array
 char ** Command::getArr() {
     return argArr;
 }
 
+// sets pass to true
 void Command::passed() {
     pass = true;
     return;
 }
 
+// sets pass to false
 void Command::failed() {
     pass = false;
     return;
 }
 
+// returns pass
 bool Command::getPass() {
     return pass;
 }
